a0e097ca522f2b45a4986be528eb4e1df06da96a
Cyber_User <eitanforschool08@gmail.com>
2026-01-29 18:12:06 +0200

    Reworking

--- ViewModel/ViewModelPage/MainWindowViewModel.cs
+++ ViewModel/ViewModelPage/MainWindowViewModel.cs
@@ -8,12 +8,12 @@ public partial class MainWindowViewModel : ObservableObject, INavigatable
     private readonly INavigationService _navigationService;
     private readonly IDisposable _navContext;
 
-    [ObservableProperty] private INavigatable _activeSubPage;
+    [ObservableProperty] private INavigatable _currentViewModel;
 
     public MainWindowViewModel(INavigationService navigationService)
     {
         _navigationService = navigationService;
-
+        navigationService.UseContext(vm => CurrentViewModel = vm);
         _ = _navigationService.NavigateToAsync<LoginViewModel>();
     }
 
